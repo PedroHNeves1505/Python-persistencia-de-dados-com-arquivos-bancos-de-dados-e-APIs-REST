eace81c0d95ff6aeb11f18aeb00b12424d2c031f
GrandeSasuke <158407213+GrandeSasuke@users.noreply.github.com>
2026-09-01 16:36:54 -0300

    refactor: Atualizando write_db.py e read_db.py para poder adicionar e ver valores da tabela disciplinas

--- Modulo_4_SQL_SQLite/read_db.py
+++ Modulo_4_SQL_SQLite/read_db.py
@@ -3,16 +3,27 @@ import sqlite3 as sql
 conn = sql.connect('Modulo_4_SQL_SQLite/escola.db')
 cursor = conn.cursor()
 
+# cursor.execute(
+#     """
+#         SELECT * FROM estudantes
+#     """
+# )
+
+# estudantes = cursor.fetchall()
+
 cursor.execute(
     """
-        SELECT * FROM estudantes
+        SELECT * FROM disciplinas
     """
 )
 
-estudantes = cursor.fetchall()
+disciplinas = cursor.fetchall()
 
 conn.commit()
 conn.close()
 
-for estudante in estudantes:
-    print(estudante)
+# for estudante in estudantes:
+#     print(estudante)
+
+for disciplina in disciplinas:
+    print(disciplina)
--- Modulo_4_SQL_SQLite/write_db.py
+++ Modulo_4_SQL_SQLite/write_db.py
@@ -3,11 +3,17 @@ import sqlite3 as sql
 conn = sql.connect('Modulo_4_SQL_SQLite/escola.db')
 cursor = conn.cursor()
 
+# cursor.execute(
+#     """
+#         INSERT INTO estudantes(nome, idade)\
+#         VALUES (?, ?)
+#     """, ('Pedro', 19)
+# )
+
 cursor.execute(
     """
-        INSERT INTO estudantes(nome, idade)\
-        VALUES (?, ?)
-    """, ('Pedro', 19)
+        INSERT INTO disciplinas(estudante_id, nome) VALUES (?, ?)
+    """, (1, 'Computer Science')
 )
 
 conn.commit()
